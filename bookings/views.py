b88010aa4d6c9e83c12c69ec7613ce754c1df52b
ilaria-barletta <>
2023-06-16 16:33:46 +0000

    add template and urls for homepage

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+# When users come to the site we want to show the home page 
+# We are using TemplateView so we can just render 
+# a template without any model
+class Home(generic.TemplateView):
+    template_name = "home.html"
