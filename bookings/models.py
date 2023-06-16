892a7d8f19307aede0564d7f88c9ab14a6903bae
ilaria-barletta <>
2023-06-16 16:06:31 +0000

    add booking field to review model

--- bookings/models.py
+++ bookings/models.py
@@ -5,6 +5,7 @@ from cloudinary.models import CloudinaryField
 
 BOOKING_TYPE = ((0, "Pre Visit"), (1, "Stay"))
 
+
 class Pet(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField(validators=[
@@ -14,7 +15,8 @@ class Pet(models.Model):
     breed = models.CharField(max_length=80)
     allergies = models.CharField(max_length=200)
     notes = models.CharField(max_length=200)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="pets")
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="pets")
     picture = CloudinaryField('image', default='placeholder')
 
     class Meta:
@@ -23,15 +25,21 @@ class Pet(models.Model):
     def __str__(self):
         return self.name
 
+
 class Booking(models.Model):
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="bookings")
     booking_type = models.IntegerField(choices=BOOKING_TYPE, default=0)
+
 
 class Review(models.Model):
     score = models.IntegerField(validators=[
         MaxValueValidator(5),
         MinValueValidator(1)
     ])
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews")
+    booking = models.ForeignKey(
+        Booking, related_name="booking", on_delete=models.CASCADE, null=True)
