2f56060967417e29fc5a8d814037eb3b5bc351c7
ilaria-barletta <114695123+ilaria-barletta@users.noreply.github.com>
2023-06-30 19:16:19 +0100

    show validation message for pre-visit booking

--- bookings/forms.py
+++ bookings/forms.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 
 MAX_BOOKINGS_PER_DAY = 2
 MAX_STAY_DURATION_DAYS = 30
+MAX_PRE_VISIT_DURATION_DAYS = 1
 
 
 class ReviewForm(forms.ModelForm):
@@ -41,6 +42,7 @@ class BookingForm(forms.ModelForm):
         cleaned_data = super().clean()
         start_date = cleaned_data.get("start_date")
         end_date = cleaned_data.get("end_date")
+        pet = cleaned_data.get("pet")
 
         if (end_date < start_date):
             raise forms.ValidationError(
@@ -51,10 +53,18 @@ class BookingForm(forms.ModelForm):
         dates = [start_date+datetime.timedelta(days=x) for x in range((end_date-start_date).days)]
         dates.append(end_date)
 
-        if (len(dates) > MAX_STAY_DURATION_DAYS):
-            raise forms.ValidationError(
-                        "You have chosen to book for too many days. Please choose at most 30 days"
-                    )
+        bookings_for_pet = Booking.objects.filter(pet=pet)
+        is_pre_visit = len(bookings_for_pet) < 1
+
+        # This will validate if this is the first visit for this pet 
+        # and change the maximum number of days
+        max_days = MAX_PRE_VISIT_DURATION_DAYS if is_pre_visit else MAX_STAY_DURATION_DAYS
+        pre_visit_validation_days_message = "As this pet has not had a stay before we need to have a pre-visit. Please choose at most one day for your booking"
+        full_stay_validation_message = f"You have chosen to book for too many days. Please choose at most {max_days} days"
+        max_days_message = pre_visit_validation_days_message if is_pre_visit else full_stay_validation_message
+
+        if (len(dates) > max_days):
+            raise forms.ValidationError(max_days_message)
 
         for date in dates:
             # Get all the bookings that start or end on this date
